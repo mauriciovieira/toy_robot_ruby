80e7fb8b33dc4b457a1c5c20761ca6a31c447da1
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-09-17 23:39:14 -0300

    Finish CLI interface

--- lib/maux_robot/cli.rb
+++ lib/maux_robot/cli.rb
@@ -1,12 +1,59 @@
 # frozen_string_literal: true
 
 module MauxRobot
+
   # The CLI is a class responsible of handling command line interface
   class CLI
+    ALLOWED_ORDERS = %i[left right move place report].freeze
 
-    # Executes the Toy Robot script
+    attr_reader :robot
+    def initialize
+      @robot = Robot.new
+    end
+
+    # Receives everything from standard input and executes
     def run
-      puts 'This is MauxRobot working'
+      run_script(STDIN.read)
+    end
+
+    def run_script(all_input)
+      all_input.split("\n").each do |line_input|
+        command = parse(line_input)
+        execute(command)
+      end
+    end
+
+    def parse(line_input)
+      clean_input = line_input.strip.squeeze(' ').split(' ')
+      order = sanitize_order(clean_input.shift)
+      command = { order: order }
+
+      if order && clean_input.any?
+        arguments = sanitize_arguments(clean_input.join(''))
+        command[:arguments] = arguments
+      end
+
+      command
+    end
+
+    def execute(command)
+      if command[:arguments].nil?
+        @robot.send(command[:order])
+      else
+        @robot.send(command[:order], command[:arguments])
+      end
+    end
+
+    private
+
+    def sanitize_order(input_string)
+      order = input_string.downcase.to_sym
+      order if ALLOWED_ORDERS.include?(order)
+    end
+
+    def sanitize_arguments(arguments)
+      arguments = arguments.delete(' ').split(',')
+      { x: arguments[0], y: arguments[1], face: arguments[2] }
     end
   end
 end
---
+++ spec/maux_robot/cli_spec.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'maux_robot'
+
+describe MauxRobot::CLI do
+  describe '#parse' do
+    context 'valid orders' do
+      it '#report' do
+        expect(subject.parse('REPORT')).to eq(order: :report)
+      end
+
+      it '#move' do
+        expect(subject.parse('MOVE')).to eq(order: :move)
+      end
+
+      it '#left' do
+        expect(subject.parse('LEFT')).to eq(order: :left)
+      end
+
+      it '#right' do
+        expect(subject.parse('RIGHT')).to eq(order: :right)
+      end
+
+      it '#place' do
+        expect(subject.parse('PLACE 0,3,WEST')).to eq(order: :place, arguments: { x: '0', y: '3', face: 'WEST' })
+      end
+    end
+
+    context 'orders with spaces and downcase' do
+      it '#report' do
+        expect(subject.parse('    repoRT')).to eq(order: :report)
+      end
+
+      it '#move' do
+        expect(subject.parse('move      ')).to eq(order: :move)
+      end
+
+      it '#left' do
+        expect(subject.parse('   LefT ')).to eq(order: :left)
+      end
+
+      it '#right' do
+        expect(subject.parse('   rigHt')).to eq(order: :right)
+      end
+
+      it '#place' do
+        expect(subject.parse('PLACE 2 ,  1 ,  NOrth')).to eq(order: :place, arguments: { x: '2', y: '1', face: 'NOrth' })
+      end
+    end
+
+    it 'ignores anything else' do
+      expect(subject.parse('blablabla balbla')).to eq(order: nil)
+    end
+  end
+
+  describe '#execute' do
+    context 'commands without arguments' do
+      it 'sends proper message to robot' do
+        expect(subject.robot).to receive(:move)
+        subject.execute(order: :move)
+      end
+    end
+
+    context 'place' do
+      it 'send place command along with arguments' do
+        expect(subject.robot).to receive(:place).with(x: '0', y: '2', face: 'EAST')
+        subject.execute(order: :place, arguments: { x: '0', y: '2', face: 'EAST' })
+      end
+    end
+  end
+end
